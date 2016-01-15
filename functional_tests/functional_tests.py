8769685757706c5409356dde080d4827cf6c87d3
ChChCheeeeo <cheo.roman+github@gmail.com>
2016-01-14 16:43:08 -0800

    Add first test with unittest [ci skip]

--- functional_tests/functional_tests.py
+++ functional_tests/functional_tests.py
@@ -1,6 +1,36 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_post_a_review_and_see_it_later(self):    
+
+        # Sara likes wine. So she heads overs to the wine's home page.
+        self.browser.get('http://localhost:8000')
+
+        # And notcies the wine's title in the corder along with some other
+        # details about wines choses by users along with their ratings.
+
+        assert 'RecommendWine' in self.browser.title, "Browser title was " + self.browser.title
+
+        # She notices that there's also an option to add her own wine rating and review
+
+        # She selects a rating using the raiting selector
+
+        # And adds a little bit to the comment box.
+
+        # Clicking on the Add button she notices that the reviws and ratings for her
+        # chosen wine has updated.
+
+        # Satisfied she opens up another bottle to review.
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
