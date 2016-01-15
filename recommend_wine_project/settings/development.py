5f4c255519f8472a17d3d6ec920ad90d3ce45ae5
ChChCheeeeo <cheo.roman+github@gmail.com>
2016-01-14 17:25:40 -0800

    Add db settings to development [ci skip]

--- recommend_wine_project/settings/development.py
+++ recommend_wine_project/settings/development.py
@@ -4,3 +4,16 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 TEMPLATE_DEBUG = True
+
+# Add DATABASES for travis testing
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_env_variable('DATABASE_NAME'),
+        'USER': get_env_variable('DATABASE_USER'),
+        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
+        'HOST': '',
+        'PORT': '',
+    }
+}
