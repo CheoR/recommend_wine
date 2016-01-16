484b4de94d19b3d968046a3cf2b3199827d2eeac
ChChCheeeeo <cheo.roman+github@gmail.com>
2016-01-16 07:43:38 -0800

    [ci skip] Refactor page title into its own html file.

--- reviews_app/views.py
+++ reviews_app/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    return HttpResponse('<html><title>RecommendWine</title></html>') 
+    return render(request, 'home.html')
