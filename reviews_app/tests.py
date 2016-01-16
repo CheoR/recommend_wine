a52d3c90041f57ab31628c401e575919f64542ca
ChChCheeeeo <cheo.roman+github@gmail.com>
2016-01-16 08:39:18 -0800

    [ci skip] Add different code paths for POST requests

--- reviews_app/tests.py
+++ reviews_app/tests.py
@@ -17,3 +17,12 @@ class HomePageTest(TestCase):
 
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['comment_text'] = 'A new comment'
+
+        response = home_page(request)
+
+        self.assertIn('A new comment', response.content.decode())
